1cf564119bb954e9eea7ddf9c3044060f0185515
timi <tomdvazq@gmail.com>
2023-07-26 12:06:57 -0300

    fix

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ ini_set('display_errors', 1);
 ini_set("log_errors", 1);
 ini_set("error_log", "C:/xampp/htdocs/php_apirest/php_error_log");
 
+// Controlador de rutas
+
 require_once "app/controllers/RoutesController.php";
 $index = new RoutesController();
 $index->index();
