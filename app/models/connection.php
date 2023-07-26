af31d68417f845b3ec83024ae2f5f9ae6b8b909b
timi <tomdvazq@gmail.com>
2023-07-26 12:06:33 -0300

    show errors

--- app/models/connection.php
+++ app/models/connection.php
@@ -4,6 +4,27 @@ class Connection {
     
     static public function infoDatabase(){
 
+        $infoDB = array (
+            "database" => "php_apirest",
+            "user" => "root",
+            "password" => "",
+        );
+
+        return $infoDB;
+    }
+
+    static public function connect(){
+        try{
+            $link = NEW PDO(
+                "mysql:host=localhost;dbname=" . Connection::infoDatabase()["database"],
+                Connection::infoDatabase()["user"],
+                Connection::infoDatabase()["password"],
+            );
+            $link->exec("SET NAMES UTF8");
+        } catch (PDOException $e) {
+            die("ERROR: " . $e->getMessage());
+        }
+        return $link;
     }
 
 }
--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 
+// Mostrar errores
+
+ini_set('display_errors', 1);
+ini_set("log_errors", 1);
+ini_set("error_log", "C:/xampp/htdocs/php_apirest/php_error_log");
+
 require_once "app/controllers/RoutesController.php";
 $index = new RoutesController();
 $index->index();
