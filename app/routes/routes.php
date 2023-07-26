424b985e4734218bc6fbc97c098dbbcb64bd2c67
timi <tomdvazq@gmail.com>
2023-07-26 03:20:40 -0300

    404

--- app/routes/routes.php
+++ app/routes/routes.php
@@ -1,9 +1,15 @@
 <?php
 
-$json = array(
-    'status' => 200,
-    'result' => 'success'
-);
+$routes = explode("/", $_SERVER['REQUEST_URI']);
+$routes = array_filter($routes);
 
-echo json_encode($json);
-return;
+if(empty($routes)) {
+    $json = array(
+        'status' => 404,
+        'result' => 'No se ha encontrado la ruta'
+    );
+    
+    echo json_encode($json);
+    return;
+}
+
